380f9b4fa9b2619bb6e036709d2231459502f717
Andrew Sowder <Locklandpanther2@yahoo.com>
2022-03-27 13:52:03 -0400

    challenge

--- exercise/js/app.js
+++ exercise/js/app.js
@@ -16,7 +16,7 @@ function shuffleArray(arr) {
     return arr.sort(function (a, b) { return Math.random() - 0.5 })
 }
 
-function createProblem() {
+function createProblem(operator) {
     for (let i = 0; i < 10; i++) {
         let problem = {
             leftNum: getRandomNumber(10),
@@ -27,7 +27,22 @@ function createProblem() {
             wrongAnswer4: 0
         }
 
-        problem.correctAnswer = problem.leftNum * problem.rightNum;
+        switch (operator) {
+            case '+':
+                problem.correctAnswer = problem.leftNum + problem.rightNum;
+                break;
+            case '-':
+                problem.correctAnswer = problem.leftNum - problem.rightNum;
+                break;
+            case '*':
+                problem.correctAnswer = problem.leftNum * problem.rightNum;
+                break;
+            case '/':
+                problem.correctAnswer = problem.leftNum / problem.rightNum;
+                break;
+        }
+
+
         problem.wrongAnswer2 = getRandomNumber(81);
         problem.wrongAnswer3 = getRandomNumber(81);
         problem.wrongAnswer4 = getRandomNumber(81);
@@ -40,161 +55,156 @@ function createProblem() {
         listOfProblems.push(problem);
 
     }
-
 }
 
-function displayQuestionAndAnswers() {
+
+    function displayQuestionAndAnswers(operator) {
 
 
-    const problemElem = document.getElementById('problem');
+        const problemElem = document.getElementById('problem');
 
-    listOfProblems.forEach((problem) => {
+        listOfProblems.forEach((problem) => {
 
-        let problemDesc = `${problem.leftNum} * ${problem.rightNum}`;
+            let problemDesc = `${problem.leftNum} ${operator} ${problem.rightNum}`;
 
-        problemElem.querySelector('div.expression').innerText = problemDesc;
+            problemElem.querySelector('div.expression').innerText = problemDesc;
 
-        shuffleArr = shuffleArray([problem.correctAnswer, problem.wrongAnswer2, problem.wrongAnswer3, problem.wrongAnswer4]);
+            shuffleArr = shuffleArray([problem.correctAnswer, problem.wrongAnswer2, problem.wrongAnswer3, problem.wrongAnswer4]);
 
-        const ul = document.querySelector('ul');
+            const ul = document.querySelector('ul');
 
-        shuffleArr.forEach((answer) => {
+            shuffleArr.forEach((answer) => {
 
-            const li = document.querySelector('li');
+                const li = document.querySelector('li');
 
-            if (answer === problem.correctAnswer) {
-                li.innerText = problem.correctAnswer;
-                li.setAttribute('class', 'correct')
-            } else {
+                if (answer === problem.correctAnswer) {
+                    li.innerText = problem.correctAnswer;
+                    li.setAttribute('class', 'correct')
+                } else {
 
-                li.innerText = answer;
-                li.setAttribute('class', 'wrong')
+                    li.innerText = answer;
+                    li.setAttribute('class', 'wrong')
 
-            }
+                }
 
-            ul.appendChild(li);
+                ul.appendChild(li);
+            });
+
+        });
+    }
+
+    function showHidden(element) {
+        element.classList.remove('hidden');
+        element.classList.add('show-hide');
+    }
+
+    function makeHidden(element) {
+        element.classList.remove('show-hide');
+        element.classList.add('hidden');
+    }
+
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+        const operator = '+';
+
+        createProblem(operator);
+
+        displayQuestionAndAnswers(operator);
+
+        const allAnswers = document.querySelectorAll('li');
+
+        let score = document.querySelector('span.currentScore');
+
+        let problemNum = document.querySelector('span.currentProblem');
+
+        const problemElem = document.getElementById('problem');
+
+        let header = document.querySelector('header');
+
+        let text = header.querySelector('p');
+
+
+
+        allAnswers.forEach((answer) => {
+            answer.addEventListener('click', (event) => {
+                event.preventDefault();
+
+                if (answer.classList.contains('correct')) {
+
+                    currentScore++;
+                    currentProblem++;
+
+                } else
+
+                    currentProblem++;
+
+                createProblem(operator);
+
+                displayQuestionAndAnswers(operator);
+
+                if (currentProblem > 10) {
+
+                    currentProblem = 10;
+
+                    let problemDiv = problemElem.querySelector('div.expression');
+
+                    let answerSection = document.getElementById('answers');
+
+
+
+                    makeHidden(problemDiv);
+                    makeHidden(answerSection);
+                    makeHidden(text);
+
+
+                }
+
+                score.innerText = `${currentScore}`;
+
+                problemNum.innerText = `${currentProblem}`;
+
+            });
+
+
+
+        });
+
+
+        let startOverBtn = document.querySelector('button');
+
+        startOverBtn.addEventListener('click', () => {
+
+
+            let problemDiv = problemElem.querySelector('div.expression');
+
+
+            let answerSection = document.getElementById('answers');
+
+            showHidden(problemDiv);
+
+            showHidden(answerSection);
+
+            showHidden(text);
+
+
+            currentScore = 0;
+
+            currentProblem = 1;
+
+            score.innerText = `${currentScore}`
+
+            problemNum.innerText = `${currentProblem}`
+
+            createProblem(operator);
+
+            displayQuestionAndAnswers(operator);
+
+
         });
 
     });
-}
-
-function showHidden(element) {
-    element.classList.remove('hidden');
-    element.classList.add('show-hide');
-}
-
-function makeHidden(element) {
-    element.classList.remove('show-hide');
-    element.classList.add('hidden');
-}
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    createProblem();
-
-    displayQuestionAndAnswers();
-
-    const allAnswers = document.querySelectorAll('li');
-
-    let score = document.querySelector('span.currentScore');
-
-    let problemNum = document.querySelector('span.currentProblem');
-
-    const problemElem = document.getElementById('problem');
-
-    let header = document.querySelector('header');
-
-    let text = header.querySelector('p');
-
-
-
-    allAnswers.forEach((answer) => {
-        answer.addEventListener('click', (event) => {
-            event.preventDefault();
-
-            if (answer.classList.contains('correct')) {
-
-                currentScore++;
-                currentProblem++;
-
-            } else
-
-                currentProblem++;
-
-            createProblem();
-
-            displayQuestionAndAnswers();
-
-            if (currentProblem > 10) {
-
-                currentProblem = 10;
-
-                let problemDiv = problemElem.querySelector('div.expression');
-                
-                let answerSection = document.getElementById('answers');
-
-
-
-                makeHidden(problemDiv);
-                makeHidden(answerSection);
-                makeHidden(text);
-             
-
-            }
-
-            score.innerText = `${currentScore}`;
-
-            problemNum.innerText = `${currentProblem}`;
-
-        });
-
-
-
-    });
-
-
-    let startOverBtn = document.querySelector('button');
-
-    startOverBtn.addEventListener('click', () => {
-
-
-        let problemDiv = problemElem.querySelector('div.expression');
-        
-
-        let answerSection = document.getElementById('answers');
-
-        showHidden(problemDiv);
-
-        showHidden(answerSection);
-
-        showHidden(text);
-        
-
-        currentScore = 0;
-
-        currentProblem = 1;
-
-        score.innerText = `${currentScore}`
-
-        problemNum.innerText = `${currentProblem}`
-
-        createProblem();
-
-        displayQuestionAndAnswers();
-
-
-    });
-
-
-
-
-
-});
-
-
-
 
 
 
